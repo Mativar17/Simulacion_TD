d7ffcfc5e302c10183ccdb7906d5a19db3c93bc0
Mativar17 <matyvargasg14@gmail.com>
2022-01-22 20:02:43 -0300

    Initialized repository

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :lenders, controllers: {
+    sessions: 'lenders/sessions',
+    passwords: 'lenders/passwords',
+    registrations: 'lenders/registrations'
+  }
+  devise_for :borrowers, controllers: {
+    sessions: 'borrowers/sessions',
+    passwords: 'borrowers/passwords',
+    registrations: 'borrowers/registrations'
+  }
+
+end
